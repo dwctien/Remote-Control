cf73ab01840cd7c00ff32c2cd000a624ad5e0afb
dwctien <23120173@student.hcmus.edu.vn>
2024-12-01 23:42:04 +0700

    update file_handler: show tree

---
+++ ServerApp/html_generator.h
@@ -0,0 +1,26 @@
+﻿#ifndef _HTML_GENERATOR_H_
+#define _HTML_GENERATOR_H_
+
+#include <string>
+#include <vector>
+#include <tuple>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+struct DataFrame {
+    string type; // "group" or "single"
+    vector<string> columns; // Column names
+    vector<vector<string>> data; // Row data
+};
+
+string html_table(const DataFrame& df, const string& note = "", const string& format = "center");
+
+string html_msg(const string& msg, bool status = true, bool bold_all = false);
+
+string html_tree(const string& path, const vector<string>& sub_dirs);
+
+string html_mail(const string& request, const string& content);
+
+#endif // !_HTML_GENERATOR_H_
