52baa7466d9eef51c44b9377dffc3b552fdf187d
dwctien <23120173@student.hcmus.edu.vn>
2024-12-09 09:08:04 +0700

    function separation: initialize, listen, responder

---
+++ ServerApp/ssocket.cpp
@@ -0,0 +1,178 @@
+#include "ssocket.h"
+
+void sendData(SOCKET clientSocket, const string& subject, const string& mail_body, const vector<BYTE>& mail_data) {
+    cout << "check1\n";
+    // Send the subject
+    uint32_t subjectSize = htonl(subject.size());
+    send(clientSocket, reinterpret_cast<const char*>(&subjectSize), sizeof(subjectSize), 0);
+    send(clientSocket, subject.c_str(), subject.size(), 0);
+    cout << "check2\n";
+    
+    // Send the mail_body
+    uint32_t bodySize = htonl(mail_body.size());
+    send(clientSocket, reinterpret_cast<const char*>(&bodySize), sizeof(bodySize), 0);
+    send(clientSocket, mail_body.c_str(), mail_body.size(), 0);
+    cout << "check3\n";
+    
+
+    // Send the data (image or video) after the body
+    if (!mail_data.empty()) {
+        uint32_t dataSize = htonl(mail_data.size());
+        send(clientSocket, reinterpret_cast<const char*>(&dataSize), sizeof(dataSize), 0);
+        cout << "check4\n";
+        send(clientSocket, reinterpret_cast<const char*>(mail_data.data()), mail_data.size(), 0);
+        cout << "check5\n";
+    }
+    cout << "check6\n";
+}
+
+void getInfoFromClient(string& msg, string& admin, string& request) {
+    size_t adminPos = msg.find("admin=");
+    size_t requestPos = msg.find("&request=");
+
+    if (adminPos != string::npos && requestPos != string::npos) {
+        admin = msg.substr(adminPos + 6, requestPos - adminPos - 6); // 6 is the length of "admin="
+        request = msg.substr(requestPos + 9); // 9 is the length of "&request="
+    }
+}
+
+void responder(SOCKET clientSocket, string received_data) {
+    string admin = "", request = "";
+
+    getInfoFromClient(received_data, admin, request);
+
+    cout << "Received request: " << request << "\n";
+
+    Function func;
+    vector<string> params;
+    auto parse_result = parseRequest(admin, request, func, params);
+
+    string subject = "Reply for request: " + parse_result["command"];
+    string mail_body;
+    vector<BYTE> mail_data;
+
+    if (parse_result["msg"] == "Permission denied.") {
+        mail_body = html_mail(request, html_msg("You are not allowed to control this PC.", false, true));
+    }
+    else if (!func || parse_result["msg"] != "Parse request successfully.") {
+        mail_body = html_mail(request, html_msg(parse_result["msg"], false, true));
+    }
+    else {
+        try {
+            Response content = func(params);
+            mail_body = html_mail(request, content.first);
+            mail_data = content.second;
+        }
+        catch (const exception& e) {
+            mail_body = html_mail(request, html_msg("The format of arguments might be incorrect.", false, true));
+        }
+    }
+
+    cout << "Response subject: " << subject << "\n";
+    cout << "Response mail: " << mail_body << "\n";
+
+    sendData(clientSocket, subject, mail_body, mail_data);
+    cout << "check7\n";
+}
+
+SOCKET initializeServerSocket() {
+    WSADATA wsaData;
+    SOCKET serverSocket, clientSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+
+    // Initialize Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cout << "WSAStartup failed\n";
+        return INVALID_SOCKET;
+    }
+
+    // Create a socket for the server
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        cout << "Socket creation failed\n";
+        WSACleanup();
+        return INVALID_SOCKET;
+    }
+
+    // Configure server address and port
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);
+
+    // Bind the socket to the address
+    // std::bind will call the function from the <functional> library, not from Winsock.
+    if (::bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        cout << "Bind failed\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return INVALID_SOCKET;
+    }
+
+    // Listen for incoming connections
+    if (listen(serverSocket, 3) == SOCKET_ERROR) {
+        cout << "Listen failed\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return INVALID_SOCKET;
+    }
+
+    cout << "Server is listening on port " << PORT << "...\n";
+
+    return serverSocket;
+}
+
+SOCKET listenForClient(SOCKET serverSocket) {
+    struct sockaddr_in clientAddr;
+    int clientAddrSize = sizeof(clientAddr);
+
+    // Accept a connection from a client
+    SOCKET clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
+    if (clientSocket == INVALID_SOCKET) {
+        cout << "Accept failed\n";
+        return INVALID_SOCKET;
+    }
+
+    cout << "Client connected.\n";
+    return clientSocket;
+}
+
+void runServer() {
+    // Initialize
+    SOCKET serverSocket = initializeServerSocket();
+    if (serverSocket == INVALID_SOCKET) {
+        return;
+    }
+
+    // Listen for client to connect
+    SOCKET clientSocket = listenForClient(serverSocket);
+    if (clientSocket == INVALID_SOCKET) {
+        closesocket(serverSocket);
+        WSACleanup();
+        return;
+    }
+
+    char recvBuf[1024];
+    int recvSize;
+
+    // Keep receiving request (and sending response) until client disconnects
+    while (true) {
+        recvSize = recv(clientSocket, recvBuf, sizeof(recvBuf), 0);
+
+        if (recvSize == 0) {
+            cout << "Client disconnected.\n";
+            break;
+        }
+        else if (recvSize == SOCKET_ERROR) {
+            cout << "Receive failed\n";
+            break;
+        }
+
+        string received_data(recvBuf, recvSize);
+        responder(clientSocket, received_data);
+    }
+
+    // Close the connections
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+}
--- ServerApp/ssocket.h
+++ ServerApp/ssocket.h
@@ -1,88 +1,26 @@
 #ifndef _SSOCKET_H_
 #define _SSOCKET_H_
 
-#include <winsock2.h>
 #include <iostream>
+#include <winsock2.h>
 #include <ws2tcpip.h> // For inet_pton
+#include <fstream>
+#include "request_handle.h"
 
 #pragma comment(lib, "ws2_32.lib") // Link Winsock library
 
 #define PORT 8888
 
-using namespace std;
+void sendData(SOCKET clientSocket, const string& subject, const string& mailBody, const vector<BYTE>& data);
 
-void runServer() {
-    WSADATA wsaData;
-    SOCKET serverSocket, clientSocket;
-    struct sockaddr_in serverAddr, clientAddr;
-    int clientAddrSize = sizeof(clientAddr);
+void getInfoFromClient(string& msg, string& admin, string& request);
 
-    // Initialize Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        cout << "WSAStartup failed\n";
-        return;
-    }
+void responder(SOCKET clientSocket, string received_data);
 
-    // Create a socket for the server
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == INVALID_SOCKET) {
-        cout << "Socket creation failed\n";
-        WSACleanup();
-        return;
-    }
+SOCKET initializeServerSocket();
 
-    // Configure server address and port
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(PORT);
+SOCKET listenForClient(SOCKET serverSocket);
 
-    // Bind the socket to the address
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        cout << "Bind failed\n";
-        closesocket(serverSocket);
-        WSACleanup();
-        return;
-    }
-
-    // Listen for incoming connections
-    if (listen(serverSocket, 3) == SOCKET_ERROR) {
-        cout << "Listen failed\n";
-        closesocket(serverSocket);
-        WSACleanup();
-        return;
-    }
-    cout << "Server is listening on port " << PORT << "...\n";
-
-    // Accept a connection from a client
-    clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
-    if (clientSocket == INVALID_SOCKET) {
-        cout << "Accept failed\n";
-        closesocket(serverSocket);
-        WSACleanup();
-        return;
-    }
-    cout << "Client connected.\n";
-
-    char recvBuf[1024];
-    int recvSize;
-
-    // Receive data from the client
-    recvSize = recv(clientSocket, recvBuf, sizeof(recvBuf), 0);
-    if (recvSize == SOCKET_ERROR) {
-        cout << "Receive failed\n";
-    }
-    else {
-        recvBuf[recvSize] = '\0'; // Null-terminate the received data
-        cout << "Received from client: " << recvBuf << "\n";
-
-        // Echo the message back to the client
-        send(clientSocket, recvBuf, recvSize, 0);
-    }
-
-    // Close the connections
-    closesocket(clientSocket);
-    closesocket(serverSocket);
-    WSACleanup();
-}
+void runServer();
 
 #endif // !_SSOCKET_H_
